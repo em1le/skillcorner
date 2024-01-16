2d2c26b328f77e27d83b2b5cbeba022edf7fa68a
Emile <emile.bettini@gmail.com>
2024-01-16 18:29:33 +0100

    add comment

--- log.py
+++ log.py
@@ -7,4 +7,5 @@ from line import Line
 
 class Log:
     def __init__(self, data: Generator[str, None, None]) -> None:
+        """ Initialize the log with generator data """
         self.lines: list["Line"] = [Line(content=line, position=index) for index, line in enumerate(data)]
